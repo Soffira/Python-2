b0475449b1bcbfba54254c70000b32734d11d504
Soffira <sofiacher7@ya.ru>
2019-08-06 16:12:02 +0500

    changes with logging

--- lesson 4/server/__main__.py
+++ lesson 4/server/__main__.py
@@ -1,6 +1,7 @@
 import yaml
 import json
 import socket
+import logging
 from argparse import ArgumentParser
 
 from actions import resolve
@@ -26,6 +27,15 @@ if args.config:
         file_config = yaml.load(file, Loader=yaml.loader)
         config.update(file_config)
         
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers = [
+        logging.FileHandler('main.log'),
+        logging.StreamHandler(),
+    ]
+)
+
 host, port = config.get('host'), config.get('port')
 
 try:
@@ -33,11 +43,11 @@ try:
     sock.bind((host, port))
     sock.listen(5)
     
-    print(f'Server started with { host }:{ port }')
+    logging.info(f'Server was started with { host }:{ port }')
     
     while True:
         client, address = sock.accept()
-        print(f'Client was detected { address[0] }:{address[1] }')
+        logging.info(f'Client was detected { address[0] }:{address[1] }')
         
         b_request = client.recv(config.get('buffersize'))
         
@@ -48,17 +58,17 @@ try:
             controller = resolve(actions_name)
             if controller:
                 try:
-                    print(f'Client send valid request {request}')
+                    logging.info(f'Client send valid request {request}')
                     response = controller(request)
                 except Exception as err:
-                    print(f'Internet server error: {err}')
+                    logging.critical(f'Internet server error: {err}')
                     response = make_response(request, 500, data='Internet server error')
             else:
-                print(f'Controller with action name {actions_name} does not exists')
+                logging.error(f'Controller with action name {actions_name} does not exists')
                 response = make_response(request, 404, 'Action not found')
         else:
-            print(f'Client send invalid request { request }')
-            response = make_response(request, 404, 'Wrong request')
+            logging.error(f'Client send invalid request { request }')
+            response = make_response(request, 400, 'Wrong request')
             
         str_response = json.dumps(response)
         client.send(str_response.encode())
